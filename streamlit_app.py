f2d598c9c43c07f706188e79499a2f701941c1d2
martin <msmith1961@mac.com>
2022-06-08 12:09:30 -0400

    adde emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title("My Parents Healtyh New Diner")
 
 streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3  & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach, and Rocket Smoothie")
+streamlit.text(" 🥑Omega 3  & Blueberry Oatmeal")
+streamlit.text("🥣Kale, Spinach, and Rocket Smoothie")
 streamlit.text("Hard Boiled from Free Range , Egg")
