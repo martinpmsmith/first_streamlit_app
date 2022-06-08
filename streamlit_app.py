d7e428096082d04ff8c4b4de0e94f12a4c299251
martin <msmith1961@mac.com>
2022-06-08 12:27:27 -0400

    added a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title("My Parents Healtyh New Diner")
 
@@ -8,3 +9,6 @@ streamlit.text("🥣Kale, Spinach, and Rocket Smoothie")
 streamlit.text("Hard Boiled from Free Range , Egg")
 streamlit.text("🥑🍞 Avocado toast")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
