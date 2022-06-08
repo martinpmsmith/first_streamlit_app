d848cf3f2bff8d4dc4f87baaa299576f0b3cc8d7
martin <msmith1961@mac.com>
2022-06-08 12:41:55 -0400

    fitered

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,9 @@ streamlit.text("🥑🍞 Avocado toast")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.multiselect('Pick a fruit', list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect('Pick a fruit', list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc(fruits_selected)
+
+streamlit.dataframe(fruits_to_show)
